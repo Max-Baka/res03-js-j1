fe78bb7009b6f6ded6b83f0b6bc02d9471873245
Max-Baka <maximelanglois@ip-172-31-20-28.eu-west-3.compute.internal>
2023-01-03 14:29:28 +0000

    apre

---
+++ Exoaprem/ex1/exo1.js
@@ -0,0 +1,28 @@
+let i = 0;
+while(i <= 10)
+{
+    console.log(i); 
+    i = i + 1;
+}
+
+let p = 25;
+while(p >= 20)
+{
+    console.log(p);
+    p = p-1 ;
+}
+
+let y = 0;
+while(y<=20)
+{
+    if(y%2===0)
+    {
+        console.log(`${y} pairs`);
+        
+    }
+    else
+    {
+        console.log(`${y} impairs`);
+    }
+    y++;
+}
---
+++ Exoaprem/ex2/exo2.js
@@ -0,0 +1,14 @@
+let y = 0;
+while(y<=10)
+{
+    if(y%2===0)
+    {
+        console.log(`${y} pairs`);
+        
+    }
+    else
+    {
+        console.log(`${y} impairs`);
+    }
+    y++;
+}
